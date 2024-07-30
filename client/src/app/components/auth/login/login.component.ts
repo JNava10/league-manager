dcfa99c209090d765494df28fbedf8c6d91a65f1
Nava <juannr2002@gmail.com>
2024-07-30 12:13:02 +0200

    HTML del formulario del login terminado

--- client/src/app/components/auth/login/login.component.ts
+++ client/src/app/components/auth/login/login.component.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
-import {TuiInputModule, TuiUnmaskHandlerModule} from "@taiga-ui/kit";
-import {ReactiveFormsModule} from "@angular/forms";
+import {TuiInputModule, TuiInputPasswordModule, TuiUnmaskHandlerModule} from "@taiga-ui/kit";
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {TuiButtonModule, TuiHintModule, TuiTextfieldControllerModule} from "@taiga-ui/core";
+import {InputTextModule} from "primeng/inputtext";
+import {PasswordModule} from "primeng/password";
+import {DividerModule} from "primeng/divider";
+import {passwordRegex} from "../../../utils/regex.constants";
+import {ToggleButtonModule} from "primeng/togglebutton";
+import {Button} from "primeng/button";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    TuiInputModule,
     ReactiveFormsModule,
-    TuiUnmaskHandlerModule
+    InputTextModule,
+    PasswordModule,
+    DividerModule,
+    ToggleButtonModule,
+    FormsModule,
+    Button,
+    TuiInputModule,
+    TuiTextfieldControllerModule,
+    TuiInputPasswordModule,
+    TuiHintModule,
+    TuiButtonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  loginForm = new FormGroup({
+    nickOrEmail: new FormControl(''),
+    password: new FormControl(''),
+  });
 }
---
+++ client/src/app/components/utils/mode-switch/mode-switch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mode-switch',
+  standalone: true,
+  imports: [],
+  templateUrl: './mode-switch.component.html',
+  styleUrl: './mode-switch.component.css'
+})
+export class ModeSwitchComponent {
+
+}
