4d77ce22549ea1452cdfb0bc5e931b7cc8af785d
Nava <juannr2002@gmail.com>
2024-09-25 03:50:43 +0200

    problema al obtener parametros desde una subruta solucionado (al fin)

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import {provideRouter, withComponentInputBinding} from '@angular/router';
+import {provideRouter, withComponentInputBinding, withRouterConfig} from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {authInterceptor} from "./helpers/interceptor/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideHttpClient(withInterceptors([authInterceptor]))]
+  providers: [provideAnimations(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding(), withRouterConfig({paramsInheritanceStrategy: 'always'})), provideHttpClient(withInterceptors([authInterceptor]))]
 };
